4e6b4d981d97176eb0927d2161d758555b2f79da
yb1 <groeninglu@yahoo.de>
2016-12-14 17:45:57 +0100

    updated tests

--- gameoflife/src/gameoflife/game_test.go
+++ gameoflife/src/gameoflife/game_test.go
@@ -1,10 +1,55 @@
-package main_test
+package main
 
 import (
-	"fmt"
 	"testing"
+	"reflect"
 )
 
-func Example() {
-
+func TestStart(t *testing.T) {
+	tests := []field{
+		field([][]string{
+		[]string{"-","-","-"},
+		[]string{"X","X","X"},
+		[]string{"-","-","-"},
+		},),
+		field([][]string{
+		[]string{"-","-","-"},
+		[]string{"-","X","X"},
+		[]string{"-","X","X"},
+		},),
+		field([][]string{
+		[]string{"-","-","-","-"},
+		[]string{"-","X","X","X"},
+		[]string{"X","X","X","-"},
+		[]string{"-","-","-","-"},
+		},),
+		
+	}
+	
+	results := []field{
+		field([][]string{
+		[]string{"-","X","-"},
+		[]string{"-","X","-"},
+		[]string{"-","X","-"},
+		},),
+		field([][]string{
+		[]string{"-","-","-"},
+		[]string{"-","X","X"},
+		[]string{"-","X","X"},
+		},),
+		field([][]string{
+		[]string{"-","-","X","-"},
+		[]string{"X","-","-","X"},
+		[]string{"X","-","-","X"},
+		[]string{"-","X","-","-"},
+		},),
+	}
+	
+	for i , test := range tests {
+		test.start(3)
+		
+			if ! reflect.DeepEqual(test, results[i]) {
+				t.Fatalf("Expected %s, but got %s", test, results[i])
+			}
+	}
 }
