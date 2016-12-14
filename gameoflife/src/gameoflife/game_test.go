7620cbbafb54251a593f2ac66812e198d8a669fe
yb1 <groeninglu@yahoo.de>
2016-12-14 18:47:37 +0100

    added changable ticks to tests
    created struct testField
    containing tick and field

--- gameoflife/src/gameoflife/game_test.go
+++ gameoflife/src/gameoflife/game_test.go
@@ -5,24 +5,43 @@ import (
 	"reflect"
 )
 
+type testField struct {
+	f field
+	tick int
+}
+
 func TestStart(t *testing.T) {
-	tests := []field{
-		field([][]string{
+
+	tests := []testField{
+		{
+		f: field([][]string{
 		[]string{"-","-","-"},
 		[]string{"X","X","X"},
 		[]string{"-","-","-"},
 		},),
-		field([][]string{
+		tick: 3},
+		{
+		f: field([][]string{
+		[]string{"-","-","-"},
+		[]string{"X","X","X"},
+		[]string{"-","-","-"},
+		},),
+		tick: 2},
+		{
+		f: field([][]string{
 		[]string{"-","-","-"},
 		[]string{"-","X","X"},
 		[]string{"-","X","X"},
 		},),
-		field([][]string{
+		tick: 3},
+		{ 
+		f:field([][]string{
 		[]string{"-","-","-","-"},
 		[]string{"-","X","X","X"},
 		[]string{"X","X","X","-"},
 		[]string{"-","-","-","-"},
 		},),
+		tick: 3},
 		
 	}
 	
@@ -31,6 +50,11 @@ func TestStart(t *testing.T) {
 		[]string{"-","X","-"},
 		[]string{"-","X","-"},
 		[]string{"-","X","-"},
+		},),
+		field([][]string{
+		[]string{"-","-","-"},
+		[]string{"X","X","X"},
+		[]string{"-","-","-"},
 		},),
 		field([][]string{
 		[]string{"-","-","-"},
@@ -46,10 +70,9 @@ func TestStart(t *testing.T) {
 	}
 	
 	for i , test := range tests {
-		test.start(3)
-		
-			if ! reflect.DeepEqual(test, results[i]) {
-				t.Fatalf("Expected %s, but got %s", test, results[i])
+		test.f.start(test.tick)
+			if ! reflect.DeepEqual(test.f, results[i]) {
+				t.Fatalf("Expected %s, but got %s", test.f, results[i])
 			}
 	}
 }
